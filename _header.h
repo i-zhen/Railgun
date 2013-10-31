402992714420a0f77a9ef8bb8a3f6c6bf82b38e6
iamzhen <iamzhenyi@gmail.com>
2013-10-31 15:51:57 +0800

    Create _header.h

---
+++ _header.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<stdlib.h>
+#include<fstream>
+
+#ifndef _LINUX_Head
+#define _LINUX_Head
+
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/resource.h>
+#include<sys/times.h>
+#include <sys/ptrace.h>
+#include <asm/ptrace-abi.h>
+
+#else
+
+#endif
